f3042585b0f31bf1d4925077bbbfdbd4784b32d1
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-10-25 13:14:03 +0300

    extra stuff and code tests

--- database/migrations/2023_10_09_103915_create_raw_data_table.php
+++ database/migrations/2023_10_09_103915_create_raw_data_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('raw_data', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('market_data_id')->constrained('market_data')->cascadeOnDelete();
             $table->string('name');
             $table->string('type');
             $table->string('kg');
